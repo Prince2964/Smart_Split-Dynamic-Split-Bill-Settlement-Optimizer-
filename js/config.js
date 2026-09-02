6c341d469ab5a9f66a0179d176e7f89e90eb26f3
Prince Pathneja <princepathneja.2006@gmail.com>
2026-09-03 01:35:00 +0530

    feat: add application configuration

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.SS = window.SS || {};
+SS.STORAGE = Object.freeze({
+    users: 'ss_users',
+    groups: 'ss_groups',
+    invitations: 'ss_invitations',
+    currentUser: 'ss_current_user'
+});
+SS.APP = Object.freeze({ name: 'SplitSmart', version: '1.4.0', demo: true });
+
